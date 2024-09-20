da43fe735e68fd86f82e90fc12d7142007e1b059
LahariSD <132733743+LahariSD@users.noreply.github.com>
2024-09-20 11:53:44 +0530

    task.py

---
+++ task.py
@@ -0,0 +1,10 @@
+emp={'eno':[1,2,3],'ename':['A','B','C'],'esal':[10000,22000,30000]}
+print("\nThe employee database is:")
+print(emp)
+prnit("........................")
+print("\n The employee name are:"emp['ename'])
+print(".........................")
+print("\nThye employee salaries are:")
+print("...........................")
+for i in emp['esal']:
+    print(i)
